7f2bfa64e18f74418e5958c7c9575c238c14523f
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-11-27 23:38:45 +0100

    Rename Main import.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import { Grid, Container } from 'semantic-ui-react';
 
-import MatterMenu from './Menu';
+import Menu from './Menu';
 import Notebook from './Notebook';
 import Laboratory from './Laboratory';
 
 class Main extends Component {
   render() {
-      return (
-          <div>
-              <MatterMenu />
-              <Container>
-              <Grid padded>
-              <Grid.Row>
-                  <Grid.Column mobile={16} tablet={16} computer={5}>
-                  <Notebook />
-                  </Grid.Column>
-                  <Grid.Column  mobile={16} tablet={16} computer={11}>
-                  <Laboratory />
-                  </Grid.Column>
-             </Grid.Row>
-             </Grid>
-             </Container>
-          </div>
-        );
-      }
-    }
+    return (
+      <div>
+        <Menu />
+        <Container>
+          <Grid padded>
+            <Grid.Row>
+              <Grid.Column mobile={16} tablet={16} computer={5}>
+                <Notebook />
+              </Grid.Column>
+              <Grid.Column  mobile={16} tablet={16} computer={11}>
+                <Laboratory />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default Main;
