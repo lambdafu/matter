6638da4d45bbba3a02e739fe249b1018daf03e63
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-11-27 19:41:52 +0100

    Remove App component.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // Redux for state management.
 import { Provider } from 'react-redux';
 import store from './store';
 
+import 'semantic-ui-css/semantic.min.css';
+import Main from './components/Main';
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Main />
   </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
