f69b497a62f9e3bd4c878336d1c3eaca404bc249
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2019-02-25 23:10:29 +0100

    Comment.

--- src/logic/predict.js
+++ src/logic/predict.js
@@ -1,5 +1,41 @@
 import solver from '@uandi/javascript-lp-solver';
 
+// Here is how this works: Linear Programming optimizes a set of variables (x) under
+// certain constraints.  In our case, we are aiming to maximize the utilization of
+// the factories in the game.  The utilization will normally be between 0 and 1, where
+// 1 means the factory is running at full capacity.
+//
+// Linear programming's goal is to maximize (opType) a weighted sum of the variables (c^T x).
+// The weights here are priorities we assign to the factories. With these weights, we
+// can favor some factory over another. Indirectly, we can favor certain items this way
+// (if the prioritized factory produces that item). Currently, we don't have any good
+// idea on how to convert priorities for items to priorities for factories, so item
+// priority is currently ignored.
+//
+// Linear programming is subject to constraints.  First and foremost, all results
+// must be non-negative. In addition to that, we can give a set of linear inequations.
+// each linear inequation specifies that a weighted sum of the variables must be
+// smaller than some value.
+// We use two sets of inequalities: One set to control item production and consumption,
+// and one set to control utiliziation of factories.
+// The utilization is controlled in a simple manner: For each factory, we add one
+// inequality: That the utilization of that factory must be at most 1. We achieve
+// this by giving that factory a weight of 1 and every other factory a weight of 0.
+// Finally, we say that this sum must be at most 1.
+// The item production and consumption is also controlled easily. For each item,
+// we weigh the utilization of each factory with the net consumption of that item.
+// (consumption minus production). This net consumption must be less or equal than the
+// current inventory of that item.
+//
+// After calculating the utilization of all factories by solving the LP problem
+// (there should always be a solution in our case, because utilization all zero
+// should satisfy all constraints), we need to calculate for each item the
+// burndown rate and time until inventory is fully consumed. The item that is fully
+// consumed first determines the time period for that this optimization calculation
+// is valid. After that time, the inventory should be set to 0, and the calculation
+// should be redone with the new inventory, until all burndown rates are negative,
+// or enough of the future is calculated.
+
 export default
 function logit() {
   const factories = {
@@ -133,5 +169,6 @@ function logit() {
 //     },
 // };
  let result = solver.Solve(model2);
+console.log(model2);
  console.log(result);
 }
